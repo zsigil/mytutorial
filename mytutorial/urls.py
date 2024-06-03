c2ceeeb3f78db89c8078b9bc833654b4024e418f
Zsigil <chickpox@freemail.hu>
2024-06-03 13:34:53 +0200

    adding 'posts' app with startapp

--- mytutorial/urls.py
+++ mytutorial/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from mytutorial import views
 
@@ -23,4 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.homepage, name="home" ),
     path('about/',views.about, name="about"),
+
+    path('posts/',include('posts.urls'))
 ]
