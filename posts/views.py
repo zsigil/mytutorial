7123b403667acf47bea886a28736b6c4cfaffb39
Zsigil <chickpox@freemail.hu>
2024-06-03 15:58:59 +0200

    named urls, slugs and post page

--- posts/views.py
+++ posts/views.py
@@ -5,3 +5,8 @@ from .models import Post
 def posts_list(request):
     posts = Post.objects.all().order_by('-date')
     return render(request, "posts/posts_list.html", {"posts": posts})
+
+
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, "posts/post_page.html", {"post": post})
