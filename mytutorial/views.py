c78959f695e30810d62b27b83824e30c8cdb9c7f
Zsigil <chickpox@freemail.hu>
2024-06-03 11:49:30 +0200

    setting up templates

--- mytutorial/views.py
+++ mytutorial/views.py
@@ -1,9 +1,12 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from django.shortcuts import render
 
 def homepage(request):
-    return HttpResponse("Hello World. I am Django")
+ #   return HttpResponse("Hello World. I am Django")
+    return render(request, 'home.html')
 
 
 def about(request):
-    return HttpResponse("This is about something.")
+#  return HttpResponse("This is about something.")
+    return render(request, 'about.html')
 
