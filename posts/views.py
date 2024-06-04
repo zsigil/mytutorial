fb68fed53dd01e5da2f7a3d774b16485d0ce52a4
Zsigil <chickpox@freemail.hu>
2024-06-04 15:37:41 +0200

    created custom post form + using foreignkey in model

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Post
+from . import forms
+
 
 # Create your views here.
 def posts_list(request):
@@ -14,4 +16,15 @@ def post_page(request, slug):
 
 @login_required(login_url="/users/login/")
 def post_new(request):
-    return render(request, "posts/post_new.html")
+    if request.method == 'POST':
+        form = forms.CreatePost(request.POST, request.FILES) #we also send images!!! request.FILES
+        if form.is_valid():
+            newpost = form.save(commit=False)
+            newpost.author = request.user
+            newpost.save()
+            return redirect('posts:list')
+
+    else:
+        form = forms.CreatePost()
+
+    return render(request, "posts/post_new.html", {"form":form})
