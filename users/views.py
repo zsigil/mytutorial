d684d50e1157c06f1244d4a0f4471681c86d2ecb
Zsigil <chickpox@freemail.hu>
2024-06-04 13:42:40 +0200

    logout

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 
 # Create your views here.
@@ -16,11 +16,18 @@ def register_view(request):
 
 
 def login_view(request):
-        if request.method == "POST":
-            form = AuthenticationForm(data=request.POST)
-            if form.is_valid():
-                login(request, form.get_user())
-                return redirect('posts:list')
-        else:
-            form = AuthenticationForm()
-        return render(request, 'users/login.html', {"form":form})
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('posts:list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {"form":form})
+
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('posts:list') 
+     
