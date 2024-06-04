fafd9daf7d58afc278ce9288178f50b0076cbfa8
Zsigil <chickpox@freemail.hu>
2024-06-04 09:52:36 +0200

    adding users app with urls

--- mytutorial/urls.py
+++ mytutorial/urls.py
@@ -26,7 +26,8 @@ urlpatterns = [
     path('',views.homepage, name="home" ),
     path('about/',views.about, name="about"),
 
-    path('posts/',include('posts.urls'))
+    path('posts/',include('posts.urls')),
+    path('users/', include('users.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
